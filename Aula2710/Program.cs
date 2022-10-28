ee4c00f4c5b8b7e794697391061a28608c671f91
DeboraRansdorf <114961172+DeboraRansdorf@users.noreply.github.com>
2022-10-28 20:42:43 -0300

    Add files via upload

---
+++ Aula2710/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Aula2710;
+Console.WriteLine("Hello, World!");
+
+Validar validador = new Validar();
+
+validador.QuantidadeCaracteres();
+
+validador.Maiusculos();
+
+validador.Minusculos();
+
+validador.QuantidadePalavras();
+
+validador.Vogais();
